a115f2b00efc7a00965faf174b6eceb80641f1a2
Brian <brian.lockerbie@gmail.com>
2022-05-02 15:02:43 -0700

    added more employee Q's

--- index.js
+++ index.js
@@ -69,3 +69,88 @@ const addManager = () => {
     console.log(manager);
   })
 };
+
+const addEmployee = () => {
+  console.log(`
+  ====================
+  Adding an employee to the team
+  ====================
+  `);
+
+  return inquirer.prompt ([
+    {
+      type:'list',
+      name: 'role',
+      message: 'Please select employee role',
+      choices: ['Engineer', 'Intern']
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of the employee?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Type in an employee name.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Please enter an employee ID.',
+      validate: nameInput => {
+        if  (isNaN(nameInput)) {
+          console.log ('Please enter an employee ID!')
+          return false; 
+        } else {
+          return true;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Type in an employee email.',
+      validate: email => {
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+        if (valid) {
+          return true;
+        } else {
+          console.log ('Please enter an email.')
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter employee GitHub username.',
+      when: (input) => input.role === 'Engineer',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter employee GitHub username.')
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'school',
+      message: 'Enter the intern school',
+      when: (input) => input.role === 'Intern',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Type in the intern school.')
+        }
+      }
+    },
+    
+  
+  ])
+}
